23d319ec6f46aaebaf01b6a4c0cbdaaa9c47c264
PuroSaichi <purosaichi@gmail.com>
2026-09-03 10:09:04 +0700

    login page 4

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -65,6 +65,19 @@
                         required>
                 </div>
 
+                 {{-- Password --}}
+                <div class="mb-6">
+                    <label for="password" class="block text-sm font-medium text-gray-700 mb-1">Password</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password"
+                        class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition"
+                        placeholder="••••••••"
+                        required
+                    >
+                </div>
+
 
 
 
