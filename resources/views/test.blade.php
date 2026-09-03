d6113428325faf3a1324c7361aa48b8fd85ea948
PuroSaichi <purosaichi@gmail.com>
2026-09-03 09:36:47 +0700

    initial commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="bg-blue-500 text-white p-4 rounded-lg">
+        Tailwind BERHASIL JALAN!
+    </div>
+</body>
+</html>
