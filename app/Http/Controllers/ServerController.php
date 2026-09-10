4efe26cd179a312c224a18e778c1675bf85de0fe
PuroSaichi <purosaichi@gmail.com>
2026-09-10 14:53:07 +0700

    ini tuh kayak cuman commit biasa aja yang bahkan gw gak tau kenapa gw harus commit ini tapi gapapa yang penting git hub gw ijo aja dan yaa.... biar keliatan sibuk aja sih sebenernya kan seru nih tauntuing-taunting sebelah yang pas ngetik nya lama banget kayak dinosaurus.. seru jir ngetik gak jelas gini mungkin juga ini jadi commit terpanjang gw kalo ya? gila gak pernah gw sepanjang ini. BYE

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -32,10 +32,18 @@ class ServerController extends Controller
         return [
             ['id' => 1, 'name' => 'SV-R - 001', 'ip' => '103.231.3.01', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '20d 12h'],
             ['id' => 2, 'name' => 'SV-R - 002', 'ip' => '103.231.3.02', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '12d 22h'],
-            ['id' => 3, 'name' => 'SV-R - 003', 'ip' => '103.231.3.03', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '9d 11h'],
+            ['id' => 3, 'name' => 'SV-R - 003', 'ip' => '103.231.3.03', 'status' => 'Offline', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '9d 11h'],
             ['id' => 4, 'name' => 'SV-R - 004', 'ip' => '103.231.3.04', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '32d 1h'],
             ['id' => 5, 'name' => 'SV-R - 005', 'ip' => '103.231.3.05', 'status' => 'Offline', 'cpu' => '---', 'ram' => '---', 'disk' => '---', 'uptime' => '---'],
             ['id' => 6, 'name' => 'SV-R - 006', 'ip' => '103.231.3.06', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '9d 11h'],
+            ['id' => 7, 'name' => 'SV-R - 007', 'ip' => '103.231.3.06', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '9d 11h'],
+            ['id' => 8, 'name' => 'SV-R - 008', 'ip' => '103.231.3.06', 'status' => 'Offline', 'cpu' => '---', 'ram' => '---', 'disk' => '---', 'uptime' => '---'],
+            ['id' => 9, 'name' => 'SV-R - 009', 'ip' => '103.231.3.06', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '9d 11h'],
+            ['id' => 10, 'name' => 'SV-R - 010', 'ip' => '103.231.3.06', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '9d 11h'],
+            ['id' => 11, 'name' => 'SV-R - 011', 'ip' => '103.231.3.06', 'status' => 'Offline', 'cpu' => '---', 'ram' => '---', 'disk' => '---', 'uptime' => '---'],
+            ['id' => 12, 'name' => 'SV-R - 012', 'ip' => '103.231.3.06', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '9d 11h'],
+            ['id' => 13, 'name' => 'SV-R - 013', 'ip' => '103.231.3.06', 'status' => 'Online', 'cpu' => '50%', 'ram' => '70%', 'disk' => '90%', 'uptime' => '9d 11h'],
+            ['id' => 14, 'name' => 'SV-R - 014', 'ip' => '103.231.3.06', 'status' => 'Offline', 'cpu' => '---', 'ram' => '---', 'disk' => '---', 'uptime' => '---'],
         ];
     }
 }
