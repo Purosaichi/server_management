b7bbc6c54d5287ec87fb41dd139475d1f71fec31
PuroSaichi <purosaichi@gmail.com>
2026-09-17 12:49:20 +0700

    modifying detail licenses

--- app/Http/Controllers/licensecontroller.php
+++ app/Http/Controllers/licensecontroller.php
@@ -6,20 +6,53 @@ class LicenseController extends Controller
 {
     public function index()
     {
-        $licenses = [
+        $licenses = $this->licenses();
+
+        return view('pages.licenses.licenses', compact('licenses'));
+    }
+
+    public function show(int $id)
+    {
+        $license = collect($this->licenses())->firstWhere('id', $id);
+
+        abort_unless($license, 404);
+
+        // Tambahin data detail
+        $license = array_merge($license, [
+            'start_date' => '01 Januari 2026',
+            'license_number' => 'LIC-' . strtoupper(substr(md5($license['name']), 0, 8)),
+            'license_type' => $license['status'] === 'Aman' ? 'Perpetual' : 'Subscription',
+            'last_payment' => '15 Agustus 2026',
+            'next_due_date' => $license['expired'],
+            'amount' => '250.000',
+            'payment_method' => 'Transfer bank',
+            'asset_name' => 'SVR-001',
+            'hostname' => 'svr-001.kemendik.local',
+            'ip_address' => '103.231.3.01',
+            'location' => 'Data Center, Rack A01',
+            'pic' => 'Fathier Assyarief',
+        ]);
+
+        return view('pages.licenses.detail', compact('license'));
+    }
+
+    private function licenses(): array
+    {
+        return [
             [
-                'logo' => 'SWL_server.png',
-                'name' => 'Microsoft SQL Server Standard',
+                'id' => 1,
+                'name' => 'Microsoft SQL Server',
                 'provider' => 'Microsoft',
                 'status' => 'Akan Expired',
                 'status_type' => 'warning',
                 'jumlah' => '2 License',
                 'expired' => '20 Sep 2026',
                 'expired_note' => '(38 Hari Lagi)',
-                'expired_type' => 'warning'
+                'expired_type' => 'warning',
+                'logo' => 'microsoft.png',
             ],
             [
-                'logo' => 'DJango.png',
+                'id' => 2,
                 'name' => 'Django',
                 'provider' => 'Django Software Foundation',
                 'status' => 'Aman',
@@ -27,10 +60,11 @@ class LicenseController extends Controller
                 'jumlah' => '4 License',
                 'expired' => '15 Maret 2027',
                 'expired_note' => '',
-                'expired_type' => 'safe'
+                'expired_type' => 'safe',
+                'logo' => 'DJango.png',
             ],
             [
-                'logo' => 'microsoft.png',
+                'id' => 3,
                 'name' => 'Microsoft 365',
                 'provider' => 'Microsoft',
                 'status' => 'Aman',
@@ -38,10 +72,11 @@ class LicenseController extends Controller
                 'jumlah' => '7 License',
                 'expired' => '26 Mei 2027',
                 'expired_note' => '',
-                'expired_type' => 'safe'
+                'expired_type' => 'safe',
+                'logo' => 'microsoft.png',
             ],
             [
-                'logo' => 'VMware.png',
+                'id' => 4,
                 'name' => 'VMware vSphere Enterprise Plus',
                 'provider' => 'VMware vSphere Foundation',
                 'status' => 'Expired',
@@ -49,10 +84,11 @@ class LicenseController extends Controller
                 'jumlah' => '3 License',
                 'expired' => '20 Agustus 2026',
                 'expired_note' => '(Segera Bayar)',
-                'expired_type' => 'expired'
+                'expired_type' => 'expired',
+                'logo' => 'VMware.png',
             ],
             [
-                'logo' => 'oracle.png',
+                'id' => 5,
                 'name' => 'Oracle Database Enterprise Edition',
                 'provider' => 'Oracle',
                 'status' => 'Akan Expired',
@@ -60,10 +96,11 @@ class LicenseController extends Controller
                 'jumlah' => '10 License',
                 'expired' => '7 Oktober 2026',
                 'expired_note' => '(48 Hari Lagi)',
-                'expired_type' => 'warning'
+                'expired_type' => 'warning',
+                'logo' => 'oracle.png',
             ],
             [
-                'logo' => 'adobe.png',
+                'id' => 6,
                 'name' => 'Adobe Acrobat Pro',
                 'provider' => 'Adobe',
                 'status' => 'Aman',
@@ -71,154 +108,9 @@ class LicenseController extends Controller
                 'jumlah' => '8 License',
                 'expired' => '30 Juni 2027',
                 'expired_note' => '',
-                'expired_type' => 'safe'
-            ],
-        ];
-
-        return view('pages.licenses.licenses', compact('licenses'));
-    }
-
-    public function show($id)
-    {
-        $licenses = [
-            1 => [
-                'logo' => 'SWL_server.png',
-                'name' => 'Microsoft SQL Server Standard',
-                'provider' => 'Microsoft',
-                'status' => 'Akan Expired',
-                'jumlah' => '2 License',
-                'expired' => '20 Sep 2026 (38 Hari Lagi)',
-                'pic' => 'Fathier Assyarief',
-                'digunakan_oleh' => 'SVR-001, SVR-002',
-                'keterangan' => 'Lisensi digunakan untuk database server utama.',
-                'start_date' => '14 Agustus 2025',
-                'last_payment' => '14 Agustus 2025',
-                'next_due_date' => '20 September 2026',
-                'amount' => '267.000',
-                'payment_method' => 'Transfer bank',
-                'license_number' => 'MSQL-STD-2025-ABCD1234',
-                'license_type' => 'Subscription',
-                'asset_name' => 'Server Database Utama',
-                'hostname' => 'DB-PROD-01',
-                'ip_address' => '10.10.10.5',
-                'location' => 'Data Center Jakarta',
-            ],
-            2 => [
-                'logo' => 'DJango.png',
-                'name' => 'Django',
-                'provider' => 'Django Software Foundation',
-                'status' => 'Aman',
-                'jumlah' => '4 License',
-                'expired' => '15 Maret 2027',
-                'pic' => 'Fathier Assyarief',
-                'digunakan_oleh' => 'APP-001, APP-002',
-                'keterangan' => 'Framework aplikasi yang digunakan untuk pengembangan sistem.',
-                'start_date' => '15 Maret 2026',
-                'last_payment' => '15 Maret 2026',
-                'next_due_date' => '15 Maret 2027',
-                'amount' => '150.000',
-                'payment_method' => 'Transfer bank',
-                'license_number' => 'DJANGO-OPEN-SOURCE',
-                'license_type' => 'Open source',
-                'asset_name' => 'Application Platform',
-                'hostname' => 'APP-PROD-01',
-                'ip_address' => '10.10.20.10',
-                'location' => 'Data Center Jakarta',
-            ],
-            3 => [
-                'logo' => 'microsoft.png',
-                'name' => 'Microsoft 365',
-                'provider' => 'Microsoft',
-                'status' => 'Aman',
-                'jumlah' => '7 License',
-                'expired' => '26 Mei 2027',
-                'pic' => 'Fathier Assyarief',
-                'digunakan_oleh' => 'USR-001 sampai USR-007',
-                'keterangan' => 'Lisensi produktivitas untuk kebutuhan operasional kantor.',
-                'start_date' => '26 Mei 2026',
-                'last_payment' => '26 Mei 2026',
-                'next_due_date' => '26 Mei 2027',
-                'amount' => '1.200.000',
-                'payment_method' => 'Transfer bank',
-                'license_number' => 'M365-BUSINESS-2026',
-                'license_type' => 'Subscription',
-                'asset_name' => 'Office User Accounts',
-                'hostname' => 'M365-TENANT',
-                'ip_address' => 'Cloud service',
-                'location' => 'Microsoft Cloud',
-            ],
-            4 => [
-                'logo' => 'VMware.png',
-                'name' => 'VMware vSphere Enterprise Plus',
-                'provider' => 'VMware vSphere Foundation',
-                'status' => 'Expired',
-                'jumlah' => '3 License',
-                'expired' => '20 Agustus 2026 (Segera Bayar)',
-                'pic' => 'Fathier Assyarief',
-                'digunakan_oleh' => 'SVR-001 sampai SVR-003',
-                'keterangan' => 'Lisensi virtualisasi untuk infrastruktur server.',
-                'start_date' => '20 Agustus 2025',
-                'last_payment' => '20 Agustus 2025',
-                'next_due_date' => '20 Agustus 2026',
-                'amount' => '8.500.000',
-                'payment_method' => 'Transfer bank',
-                'license_number' => 'VMW-VSF-2025-001',
-                'license_type' => 'Subscription',
-                'asset_name' => 'Virtualization Cluster',
-                'hostname' => 'VMWARE-CLUSTER-01',
-                'ip_address' => '10.10.30.10',
-                'location' => 'Data Center Jakarta',
-            ],
-            5 => [
-                'logo' => 'oracle.png',
-                'name' => 'Oracle Database Enterprise Edition',
-                'provider' => 'Oracle',
-                'status' => 'Akan Expired',
-                'jumlah' => '10 License',
-                'expired' => '7 Oktober 2026 (48 Hari Lagi)',
-                'pic' => 'Fathier Assyarief',
-                'digunakan_oleh' => 'DB-001 sampai DB-010',
-                'keterangan' => 'Lisensi database untuk aplikasi layanan utama.',
-                'start_date' => '7 Oktober 2025',
-                'last_payment' => '7 Oktober 2025',
-                'next_due_date' => '7 Oktober 2026',
-                'amount' => '12.000.000',
-                'payment_method' => 'Transfer bank',
-                'license_number' => 'ORA-DB-ENT-2025-001',
-                'license_type' => 'Subscription',
-                'asset_name' => 'Enterprise Database Cluster',
-                'hostname' => 'ORA-PROD-01',
-                'ip_address' => '10.10.40.10',
-                'location' => 'Data Center Jakarta',
-            ],
-            6 => [
+                'expired_type' => 'safe',
                 'logo' => 'adobe.png',
-                'name' => 'Adobe Acrobat Pro',
-                'provider' => 'Adobe',
-                'status' => 'Aman',
-                'jumlah' => '8 License',
-                'expired' => '30 Juni 2027',
-                'pic' => 'Fathier Assyarief',
-                'digunakan_oleh' => 'USR-008 sampai USR-015',
-                'keterangan' => 'Lisensi pengolahan dokumen PDF untuk pengguna terkait.',
-                'start_date' => '30 Juni 2026',
-                'last_payment' => '30 Juni 2026',
-                'next_due_date' => '30 Juni 2027',
-                'amount' => '4.000.000',
-                'payment_method' => 'Transfer bank',
-                'license_number' => 'ADOBE-ACROBAT-2026',
-                'license_type' => 'Subscription',
-                'asset_name' => 'Document Management Users',
-                'hostname' => 'ADOBE-ADMIN',
-                'ip_address' => 'Cloud service',
-                'location' => 'Adobe Cloud',
             ],
         ];
-
-        abort_unless(isset($licenses[(int) $id]), 404);
-
-        $license = array_merge(['id' => (int) $id], $licenses[(int) $id]);
-
-        return view('pages.licenses.detail', compact('license'));
     }
 }
--- resources/views/pages/licenses/detail.blade.php
+++ resources/views/pages/licenses/detail.blade.php
@@ -1,73 +1,94 @@
 @extends('layouts.app')
 
 @section('title', 'Detail ' . $license['name'])
-@section('page-title', 'Licenses Page')
+@section('page-title', 'Detail Lisensi')
 
 @section('content')
-<link rel="stylesheet" href="{{ asset('css/licenses.css') }}">
+{{-- Pake licenses-detail.css (bukan licenses.css) --}}
+<link rel="stylesheet" href="{{ asset('css/licenses-detail.css') }}">
 
-<!-- Tombol Back -->
+{{-- Tombol Back --}}
 <div class="lic-back-wrap">
     <a href="{{ route('licenses.licenses') }}" class="lic-back">← Back</a>
 </div>
 
-<!-- ============ HEADER CARD ============ -->
+{{-- ============ HEADER CARD ============ --}}
 <div class="lic-detail-header">
     <div class="lic-detail-logo">
-        <img src="{{ asset('images/' . $license['logo']) }}" alt="{{ $license['name'] }}" class="lic-detail-logo-img">
-        <span class="lic-detail-logo-text">{{ $license['name'] }}</span>
+        <div class="lic-detail-logo-image">
+            <img src="{{ asset('images/' . $license['logo']) }}" alt="{{ $license['name'] }}" class="lic-detail-logo-img">
+        </div>
+        <div class="lic-detail-logo-content">
+            <span class="lic-detail-label">Nama Lisensi</span>
+            <span class="lic-detail-logo-text">{{ $license['name'] }}</span>
+        </div>
     </div>
 
-    <div class="lic-detail-info">
-        <div class="lic-detail-info-item">
-            <span class="lic-detail-label">Provider</span>
-            <div class="lic-detail-provider">
-                <img src="{{ asset('images/' . ($license['name'] === 'Microsoft 365' ? 'microsoft.png' : $license['logo'])) }}" alt="{{ $license['provider'] }}" class="lic-provider-logo">
-                <div>
-                    <p class="lic-provider-name">{{ $license['provider'] }}</p>
-                    <span class="lic-status-badge {{ $license['status'] === 'Aman' ? 'lic-status-safe' : ($license['status'] === 'Expired' ? 'lic-status-expired' : 'lic-status-warning') }}">{{ $license['status'] }}</span>
-                </div>
+    <div class="lic-detail-info-item">
+        <span class="lic-detail-label">Provider</span>
+        <div class="lic-detail-provider">
+            <div>
+                <p class="lic-provider-name">{{ $license['provider'] }}</p>
+                <span class="lic-status-badge 
+                    {{ $license['status'] === 'Aman' ? 'lic-status-safe' : '' }}
+                    {{ $license['status'] === 'Expired' ? 'lic-status-expired' : '' }}
+                    {{ $license['status'] === 'Akan Expired' ? 'lic-status-warning' : '' }}
+                ">
+                    {{ $license['status'] }}
+                </span>
             </div>
         </div>
+    </div>
 
-        <div class="lic-detail-info-item">
-            <span class="lic-detail-label">Tanggal Mulai</span>
-            <span class="lic-detail-value-lg">{{ $license['start_date'] }}</span>
-        </div>
+    <div class="lic-detail-info-item">
+        <span class="lic-detail-label">Tanggal Mulai</span>
+        <span class="lic-detail-value-lg">{{ $license['start_date'] }}</span>
+    </div>
 
-        <div class="lic-detail-info-item">
-            <span class="lic-detail-label">Tanggal Berakhir</span>
-            <span class="lic-detail-value-lg {{ $license['status'] === 'Aman' ? 'lic-text-green' : ($license['status'] === 'Expired' ? 'lic-text-danger' : 'lic-text-warning') }}">{{ $license['expired'] }}</span>
-        </div>
+    <div class="lic-detail-info-item">
+        <span class="lic-detail-label">Tanggal Berakhir</span>
+        <span class="lic-detail-value-lg 
+            {{ $license['status'] === 'Aman' ? 'lic-text-green' : '' }}
+            {{ $license['status'] === 'Expired' ? 'lic-text-danger' : '' }}
+            {{ $license['status'] === 'Akan Expired' ? 'lic-text-warning' : '' }}
+        ">
+            {{ $license['expired'] }}
+        </span>
     </div>
 </div>
 
-<!-- ============ 3 CARD INFO ============ -->
+{{-- ============ 3 CARD INFO ============ --}}
 <div class="lic-info-grid">
 
-    <!-- Billing Information -->
+    {{-- Billing Information --}}
     <div class="lic-info-card">
         <div class="lic-info-header">
-            <h4><i class="fas fa-file-invoice"></i> Billing Information</h4>
-            <span class="lic-info-status">Status: <strong class="lic-text-green">{{ $license['status'] === 'Expired' ? 'Unpaid' : 'Paid' }}</strong></span>
+            <h4>Billing Information</h4>
+            <span class="lic-info-status">
+                Status: <strong class="{{ $license['status'] === 'Expired' ? 'lic-text-danger' : 'lic-text-green' }}">
+                    {{ $license['status'] === 'Expired' ? 'Unpaid' : 'Paid' }}
+                </strong>
+            </span>
         </div>
         <ul class="lic-info-list">
             <li><span>Billing Period</span><span>1 Tahun</span></li>
             <li><span>Last payment</span><span>{{ $license['last_payment'] }}</span></li>
             <li><span>Next due date</span><span>{{ $license['next_due_date'] }}</span></li>
-            <li><span>Amount</span><span>{{ $license['amount'] }}</span></li>
+            <li><span>Amount</span><span>Rp{{ $license['amount'] }}</span></li>
             <li><span>Payment method</span><span>{{ $license['payment_method'] }}</span></li>
         </ul>
     </div>
 
-    <!-- Licenses Information -->
+    {{-- Licenses Information --}}
     <div class="lic-info-card">
         <div class="lic-info-header">
-            <h4><i class="fas fa-key"></i> Licenses Information</h4>
-            <span class="lic-info-status">Status: <strong class="lic-text-green">Active</strong></span>
+            <h4>Licenses Information</h4>
+            <span class="lic-info-status">
+                Status: <strong class="lic-text-green">Active</strong>
+            </span>
         </div>
         <ul class="lic-info-list">
-            <li><span>ID License</span><span>LIC-{{ $license['id'] }}-2025</span></li>
+            <li><span>ID License</span><span>LIC-{{ $license['id'] }}-2026</span></li>
             <li><span>Nama Lisensi</span><span>{{ $license['name'] }}</span></li>
             <li><span>Nomor Lisensi</span><span>{{ $license['license_number'] }}</span></li>
             <li><span>Jumlah Lisensi</span><span>{{ $license['jumlah'] }}</span></li>
@@ -75,11 +96,13 @@
         </ul>
     </div>
 
-    <!-- Asset Information -->
+    {{-- Asset Information --}}
     <div class="lic-info-card">
         <div class="lic-info-header">
-            <h4><i class="fas fa-server"></i> Asset Information</h4>
-            <span class="lic-info-status">Status: <strong class="lic-text-green">Active</strong></span>
+            <h4>Asset Information</h4>
+            <span class="lic-info-status">
+                Status: <strong class="lic-text-green">Active</strong>
+            </span>
         </div>
         <ul class="lic-info-list">
             <li><span>Nama Aset</span><span>{{ $license['asset_name'] }}</span></li>
@@ -92,7 +115,7 @@
 
 </div>
 
-<!-- ============ TABEL RIWAYAT PEMBAYARAN ============ -->
+{{-- ============ TABEL RIWAYAT PEMBAYARAN ============ --}}
 <div class="lic-payment-card">
     <table class="lic-payment-table">
         <thead>
@@ -104,18 +127,24 @@
             </tr>
         </thead>
         <tbody>
-            @php
-                preg_match('/(\d{4})$/', $license['last_payment'], $paymentYearMatch);
-                $lastPaymentYear = (int) ($paymentYearMatch[1] ?? date('Y'));
-            @endphp
-            @for ($yearOffset = 0; $yearOffset < 3; $yearOffset++)
-                <tr>
-                    <td>{{ preg_replace('/\d{4}$/', (string) ($lastPaymentYear - $yearOffset), $license['last_payment']) }}</td>
-                    <td>Rp{{ $license['amount'] }}</td>
-                    <td>Transfer bank</td>
-                    <td><span class="lic-payment-status">Paid</span></td>
-                </tr>
-            @endfor
+            <tr>
+                <td>15 Agustus 2026</td>
+                <td>Rp{{ $license['amount'] }}</td>
+                <td>Transfer bank</td>
+                <td><span class="lic-payment-status">Paid</span></td>
+            </tr>
+            <tr>
+                <td>15 Agustus 2025</td>
+                <td>Rp{{ $license['amount'] }}</td>
+                <td>Transfer bank</td>
+                <td><span class="lic-payment-status">Paid</span></td>
+            </tr>
+            <tr>
+                <td>15 Agustus 2024</td>
+                <td>Rp{{ $license['amount'] }}</td>
+                <td>Transfer bank</td>
+                <td><span class="lic-payment-status">Paid</span></td>
+            </tr>
         </tbody>
     </table>
 </div>
